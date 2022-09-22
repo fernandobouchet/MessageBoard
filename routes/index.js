4449bd58c075d1d69fd36a324be738d0078132ac
fernandobouchet <fernandobouchet@gmail.com>
2022-09-22 20:08:21 -0300

    Add minimal changes to display messages on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  { text: 'Hi there!', user: 'Fernando', added: new Date() },
+  { text: 'Hi yo!', user: 'Fernando', added: new Date() },
+  { text: 'Hi Fer!', user: 'Fernando', added: new Date() },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
